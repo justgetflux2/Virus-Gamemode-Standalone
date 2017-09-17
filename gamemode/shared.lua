29a46c69480f0b0a7a5ac1f956c9840ebade8ea3
rollersteaam <rollersteaam@googlemail.com>
2017-09-17 23:12:30 +0100

    Unstaging all lua files in preparation for framework transit.

--- gamemode/shared.lua
+++
@@ -1,8 +0,0 @@
-DeriveGamemode("base")
-MINIMUM_PLAYER_AMOUNT = 4	//Minimum amount of players to start a game
-TimeLimit = 15 * 60		//15 minutes until end of round
-
-GM.Name     = "Virus"
-GM.Author   = "Sphere / Flux / Rollersteaam"
-GM.Email 	= ""
-GM.Website 	= ""
